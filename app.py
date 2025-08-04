9739e61aba65f07d86c946d296bde8eba96432d7
metehan876161-oss <metehan876161@gmail.com>
2025-08-05 00:01:15 +0300

    app.py

--- app.py
+++ app.py
@@ -66,4 +66,6 @@ def analiz90_api():
     return jsonify(sonuc)
 
 if __name__ == "__main__":
-    app.run()
+    import os
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
